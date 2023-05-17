41269f4b53b2b8c3241d0cf7e07d3adcbbefaf05
Tony Gorez <gorez.tony@gmail.com>
2023-05-17 19:25:39 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Tony Gorez on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
