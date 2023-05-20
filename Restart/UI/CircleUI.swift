4a0ccf4701579de42e221fc306922c2cddf39b05
Tony Gorez <gorez.tony@gmail.com>
2023-05-20 20:25:13 +0200

    feat: add circle animation

--- Restart/UI/CircleUI.swift
+++ Restart/UI/CircleUI.swift
@@ -11,6 +11,8 @@ struct CircleUI: View {
     @State var ShapeColor: Color
     @State var ShapeOpacity: Double
     
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
         ZStack {
             Circle()
@@ -20,6 +22,14 @@ struct CircleUI: View {
                 .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
                 .frame(width: 260, height: 260, alignment: .center)
         } //: ZStack
+        .blur(radius: isAnimating ? 0 : 5)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1), value: isAnimating)
+        .onAppear(perform: {
+            isAnimating = true
+        })
+        
     }
 }
 
