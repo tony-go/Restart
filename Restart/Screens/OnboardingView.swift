e18d101d61a87ee69658407a034ce11215029425
Tony Gorez <gorez.tony@gmail.com>
2023-05-21 15:20:38 +0200

    feat: add indicator for gesture

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -16,6 +16,7 @@ struct OnboardingView: View {
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
     @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1
     
     private func goToHome() {
         isOnboardingViewActive = false
@@ -72,14 +73,30 @@ struct OnboardingView: View {
                                 .onChanged{ gesture in
                                     if abs(imageOffset.width) <= 150 {
                                         imageOffset = gesture.translation
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                        }
                                     }
                                 }
                                 .onEnded { _ in
                                     imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                    }
                                 }
                         )
                         .animation(.easeOut(duration: 0.5), value: imageOffset)
                 } //: Body
+                .overlay(
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(1), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom
+                )
                 Spacer()
 
                 // MARK: - BODY
