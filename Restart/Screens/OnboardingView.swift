1d6ee3c149855723a7bba1bceaba8b931b149dbd
Tony Gorez <gorez.tony@gmail.com>
2023-05-21 15:07:05 +0200

    feat: add rotation effect

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -56,6 +56,9 @@ struct OnboardingView: View {
                 // MARK: - BODY
                 ZStack {
                     CircleUI(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1, y: 0)
+                        .blur(radius: abs(imageOffset.width / 10))
+                        .animation(.easeOut(duration: 0.5), value: imageOffset)
 
                     Image("character-1")
                         .resizable()
@@ -63,6 +66,7 @@ struct OnboardingView: View {
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
                         .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
                         .gesture(
                             DragGesture()
                                 .onChanged{ gesture in
